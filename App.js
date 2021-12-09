0bde583d0661ae18c3574db2eab6ad9c7b75e748
Baleksas <alexasbag17@gmail.com>
2021-12-09 12:31:05 +0000

    fix: removing comments before merge

--- App.js
+++ App.js
@@ -1,47 +1,3 @@
-// import "react-native-gesture-handler";
-
-// import * as React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import Geo from "./screens/Geo";
-// import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
-// import { ScrollView } from "react-native-gesture-handler";
-// import HomeScreen from "./screens/HomeScreen";
-// import LoginScreen from "./screens/LoginScreen";
-
-// function DetailsScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Details Screen</Text>
-//     </View>
-//   );
-// }
-
-// const Stack = createNativeStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           options={{ headerShown: false }}
-//           name="Login"
-//           component={LoginScreen}
-//         />
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//     // <NavigationContainer>
-//     //   <Stack.Navigator>
-//     //     <Stack.Screen name="Home" component={HomeScreen} />
-//     //     <Stack.Screen name="Geo" component={Geo} />
-//     //     <Stack.Screen name="Details" component={DetailsScreen} />
-//     //   </Stack.Navigator>
-//     // </NavigationContainer>
-//   );
-// }
-
-// export default App;
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -61,7 +17,6 @@ export default function App() {
           name="Login"
           component={LoginScreen}
         />
-        <Stack.Screen name="Geo" component={Geo} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
