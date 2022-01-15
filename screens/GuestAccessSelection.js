1d931a14539d6a89c35ca35298e01c139e6d4309
chloejassur <C.M.Jassur2@newcastle.ac.uk>
2022-01-15 13:19:24 +0000

    Corrected nav bar placement for ios in GuestAccessSelection.js

--- screens/GuestAccessSelection.js
+++ screens/GuestAccessSelection.js
@@ -8,7 +8,7 @@ import {
     View,
     TouchableOpacity,
     ScrollView,
-    Dimensions, Image,
+    Dimensions, Image, Platform,
 } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 const SelectionScreen = () => {
@@ -154,7 +154,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: "row",
         position: "absolute",
-        bottom: 0,
+        top: Platform.OS === 'ios'? Dimensions.get("window").height * 0.825: Dimensions.get("window").height * 0.865,
     },
     button: {
         backgroundColor: "#A2C23D",
