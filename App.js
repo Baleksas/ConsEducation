fa216fa18ff98558137fd733614b6aaa446ca4aa
chloejassur <C.M.Jassur2@newcastle.ac.uk>
2022-01-18 14:06:56 +0000

    Removed unused imports

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 /*jshint esversion: 6 */
 
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
